9f7c9910e86331a3c521b46dac568abbbfc4a00e
deepak123bharat <dsrdeepak8@gmail.com>
2020-09-23 10:24:17 +0530

    changes in html.lua

--- src/robodoc/outputs/html.lua
+++ src/robodoc/outputs/html.lua
@@ -40,10 +40,112 @@ end
  *   to the name of the documentation file.
  * SYNOPSIS
  ]]
+
 function createCSS(document)
 --[[
  * INPUTS
  *   o document -- the document for which to create the file.
  * SOURCE
  ]]
+end
+
+local str;
+
+
+--[[***f* HTML_Generator/RB_HTML_Generate_Div
+ * FUNCTION
+ *   Write a Div to the destination document 
+ * SYNOPSIS
+ ]]
+
+function generateDiv(id)
+--[[
+ * INPUTS
+ *   o id -- id Attribute of the div
+ * SEE ALSO
+ *   RB_HTML_Generate_DivEnd()
+ * SOURCE
+ ]]
+	str = str.."<div id=\""..id.."\">\n"
+end
+
+
+--[[***f* HTML_Generator/RB_HTML_Generate_DivEnd
+ * FUNCTION
+ *   Write a DivEnd to the destination document
+ * SYNOPSIS
+ ]]
+
+ function generateDivEnd(id)
+--[[
+ * INPUTS
+ *   o id -- id Attirbute of the div
+ * SEE ALSO
+ *   RB_HTML_Generate_DivEnd()
+ * SOURCE
+ ]]
+	str = str.."</div><!----"..id.."--->\n"
+end
+
+
+--[[***f* HTML_Generator/RB_HTML_Generate_String
+ * FUNCTION
+ *   Write a string to the destination document, escaping
+ *   characters where necessary.
+ * SYNOPSIS
+ ]]
+function GenerateString(a_string)
+--[[
+ * INPUTS
+ *   o a_string -- a nul terminated string.
+ * SEE ALSO
+ *   RB_HTML_Generate_Char()
+ * SOURCE
+]]
+    l = #a_string
+    for i=0,#a_string do
+        c = a_string[i];
+        GenerateChar(c);
+	end
+end
+
+
+--[[***f* HTML_Generator/RB_HTML_Generate_Char
+ * NAME
+ *   RB_HTML_Generate_Char -- generate a single character for an item.
+ * SYNOPSIS
+ ]]
+function generateChar(c)
+--[[
+ * FUNCTION
+ *   This function is called for every character that goes
+ *   into an item's body.  This escapes all the reserved
+ *   HTML characters such as '&', '<', '>', '"'.
+ * SOURCE
+ ]]
+
+ 	local switch = { 
+    	['\n'] = function()	-- for case '\n'
+        	str = str..""
+    	end,
+    	['\t'] = function()	-- for case '\t'
+        	str = str..""
+    	end,
+    	['<'] = function()	-- for case '<'
+        	str = str.."&lt"
+		end,
+		['>'] = function()  -- for case '<'
+			str = str.."&gt"
+		end,
+		['&'] = function()  -- for case '&'
+			str = str.."&amp"
+		end
+	}
+	
+	local f = switch[c]
+	if (f) then
+		f()
+	else                   --for default
+		str = str..c 
+	end
 end
