d19b99ade6cc7b79948da80ea5f742244c1f13b3
deepak123bharat <dsrdeepak8@gmail.com>
2020-09-26 18:53:43 +0530

    update in html.lua

--- src/robodoc/outputs/html.lua
+++ src/robodoc/outputs/html.lua
@@ -29,6 +29,8 @@ function init(document)
 end
 
 
+
+
 --[[***f* HTML_Generator/RB_Create_CSS
  * FUNCTION
  *   Create the .css file.  Unless the user specified it's own css
@@ -49,9 +51,11 @@ function createCSS(document)
  ]]
 end
 
-local str;
-
-
+function strcmp(a,b) return a==b end
+local str
+local in_linecomment = 0
+local SOURCE_CLASS = "source"
+local LINE_NUMBER_CLASS = "line_number"
 --[[***f* HTML_Generator/RB_HTML_Generate_Div
  * FUNCTION
  *   Write a Div to the destination document 
@@ -94,7 +98,7 @@ end
  *   characters where necessary.
  * SYNOPSIS
  ]]
-function GenerateString(a_string)
+function generateString(a_string)
 --[[
  * INPUTS
  *   o a_string -- a nul terminated string.
@@ -149,3 +153,263 @@ function generateChar(c)
 		str = str..c 
 	end
 end
+
+--[[***f* HTML_Generator/RB_HTML_Generate_Color_String
+ * NAME
+ *   RB_HTML_Color_string -- generate vairous colored string.
+ * SYNOPSIS
+ ]]
+ function colorString(open,class,a_string)
+	--[[
+	 * FUNCTION
+	 * 
+	 * SOURCE
+	 ]]
+	if(open==0) then  -- string,closing
+		generateString(a_string)
+		str=str+"</span>"
+	elseif(open==1) then   --opening, string
+		str=str+"<span class=\"" + class + "\""
+		generateString(a_string)
+	elseif(open==2) then	--opening, string, closing
+		str=str+"<span class=\"" + class + "\""
+		generateString(a_string)
+		str=str+"</span>"
+	end		 
+end
+
+--[[****f* HTML_Generator/RB_HTML_Generate_Line_Comment_End
+ * FUNCTION
+ *   Check if a line comment is active and generate ending sequence for it.
+ *   Should be called at the end of each SOURCE line.
+ * SYNOPSIS
+ ]]
+function generateLineCommentEnd()
+
+    -- Check if we are in a line comment
+    if ( in_linecomment == 1) then
+        -- and end the line comment
+        in_linecomment = 0
+        colorString(in_linecomment, COMMENT_CLASS, "" );
+	end
+end
+
+
+--[[***if* HTML_Generator/RB_HTML_Generate_Index_Shortcuts
+ * NAME
+ *   RB_HTML_Generate_Index_Shortcuts
+ * FUNCTION
+ *   Generates alphabetic shortcuts to index entries.
+ * SYNOPSIS
+ ]]
+function generateIndexShortcuts()
+	--[[
+ * INPUTS
+ *   o dest        -- the file to write to
+ * TODO
+ *   - Only list used letters.
+ *   - List all letters (accented, signs, etc), not just the common ones.
+ *   - Should be better to implement it as a <div> ?
+ * SOURCE
+ ]]
+	local c
+	str = str + "<h2>"
+
+	for c = 97,122 do
+		str = str + "<a href=\"#"+("").char(c)+"\">"
+		generateChar(c)
+		str = str + "</a> - "
+	end
+
+	for c = 0, 9 do
+		str = str + "<a href=\"#"+("").char(c)+"\">"
+		generateChar(""..c)
+		str = str + "</a>"
+		if (c != 9)
+			str = str + " - "
+		end
+	end
+
+	str = str + "</h2>\n"
+end
+
+function generateEmptyItem()
+	str = str + "<br>\n"
+end
+
+--[[***f* HTML_Generator/RB_HTML_Generate_Link
+ * NAME
+ *   RB_HTML_Generate_Link --
+ * SYNOPSIS
+ ]]
+function generateLink(cur_name,filename,labelname,linkname,classname)
+--[[
+ * INPUTS
+ *   cur_doc  --  the file to which the text is written
+ *   cur_name --  the name of the destination file
+ *                (the file from which we link)
+ *   filename --  the name of the file that contains the link
+ *                (the file we link to)
+ *   labelname--  the name of the unique label of the link.
+ *   linkname --  the name of the link as shown to the user.
+ * SOURCE
+ ]]
+	if(classname) then
+		str = str + "<a class=\""+classname + "\""
+	else
+		str = str + "<a "
+	end
+	if( filename and strcmp(filename,cur_name)) then
+		local varRelativeAddress = relativeAddress(cur_name,filename)
+		str = str + "href=\"" + varRelativeAddress + "#" + labelname + "\">"
+		generateString(linkname)
+		str = str + "</a>"
+	else
+		str = str + "href=\"#" +labelname + "\">" 
+		generateString(linkname)
+		str = str + "</a>"
+	end
+end
+
+--[[***f* HTML_Generator/RB_HTML_RelativeAddress
+ * FUNCTION
+ *   Link to 'that' from 'this' computing the relative path.  Here
+ *   'this' and 'that' are both paths.  This function is used to
+ *   create links from one document to another document that might be
+ *   in a completely different directory.
+ * SYNOPSIS
+ ]]
+function relativeAddress(thisname,thatname)
+	--[[
+ * EXAMPLE
+ *   The following two
+ *     this /sub1/sub2/sub3/f.html
+ *     that /sub1/sub2/g.html
+ *   result in 
+ *     ../g.html
+ *
+ *     this /sub1/f.html
+ *     that /sub1/sub2/g.html
+ *     ==
+ *     ./sub2/g.html
+ *
+ *     this /sub1/f.html
+ *     that /sub1/g.html
+ *     ==
+ *     ./g.html
+ *
+ *     this /sub1/doc3/doc1/tt.html
+ *     that /sub1/doc5/doc2/qq.html
+ *     ==
+ *     ../../doc5/doc2/qq.html
+ *
+ * NOTES
+ *   Notice the execelent docmentation.
+ * SOURCE
+ ]]
+	local relative = ""
+	relative[0] = '\0'
+
+	assert(thisname)
+	assert(thatname)
+
+	return relative
+	
+end
+
+
+function generateBeginContent()
+
+    generateDiv( "content" )
+end
+
+function generateEndContent()
+
+    generateDivEnd("content" )
+end
+
+function generateBeginNavigation()
+
+    generateDiv( "navigation" )
+end
+
+void generateEndNavigation()
+	generateDivEnd("navigation" )
+end
+
+function generateBeginExtra()
+	generateDiv("extra" );
+end
+
+function generateEndExtra()
+	generateDivEnd("extra")
+end
+
+
+function generateItemName(name)
+	str=str+"<p class=\"item_name\">"
+	generateString(name)
+	str=str+"</p>"
+end
+
+function insertCSS(filename) 
+	if(css_name) then
+		varRelativeAddress = relativeAddress(filename,css_name)
+		assert(varRelativeAddress)
+		assert(strlen(varRelativeAddress))
+		str = str + "<link rel=\"stylesheet\" href=\""+varRelativeAddress+"\" type=\"text/css\" />\n"
+	end
+end
+
+
+function generateBeginParagraph()
+	str=str+"<p>"
+end
+
+function generateEndParagraph()
+	str=str+"</p>\n"
+end
+
+function generateBeginPreformatted(source)
+	if(source) then
+		str=str+"<pre class=\""+SOURCE_CLASS+"\">"
+	else
+		str=str+"<pre>"
+	end
+end
+
+function generateEndPerformatted()
+	str = str + "</pre>\n"
+end
+
+function generateBeginList()
+	str = str + "<ul>"
+end
+
+function generateEndList()
+	str = str + "</ul>\n"
+end
+
+function generateBeginListItem()
+	str = str + "<li>"
+end
+
+function generateEndListItem()
+	str = str + "</li>\n"
+end
+
+
+--[[***f* HTML_Generator/RB_HTML_Generate_Item_Line_Number
+ * FUNCTION
+ *   Generate line numbers for SOURCE like items
+ * SYNOPSIS
+ ]]
+function generateItemLineNumber(lineNumberString)
+--[[
+ * INPUTS
+ *   o line_number_string -- the line number as string.
+ * SOURCE
+ ]]
+	colorString(2,LINE_NUMBER_CLASS,lineNumberString)
+end
+
